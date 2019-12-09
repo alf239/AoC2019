9e876ea9fabfde8d98f7fef2af32392055f8a2e4
Alexey Filippov <afilippov@acm.org>
2019-12-09 10:14:15 +0000

    Record for the IntState

--- intcode/src/Intcode.hs
+++ intcode/src/Intcode.hs
@@ -9,56 +9,65 @@ import Data.List
 import Data.Maybe
 import Control.Monad.State
 
-type Memory = Map Int64 Int64
-type Address = Int64
-type In = [Int64]
-type Out = [Int64]
-type IntState = (In, Out, Memory, Address, Address)
+type Value = Int64
+type Memory = Map Value Value
+type Address = Value
+type In = [Value]
+type Out = [Value]
+data IntState = IntState { input :: In
+                         , output :: Out
+                         , memory :: Memory
+                         , pc :: Address
+                         , base :: Address
+                         } deriving (Show)   
 
+
+currentOp :: IntState -> Value
+currentOp s = (memory s) ! (pc s)
+
+halted :: IntState -> Bool
+halted = (== 99) . currentOp
 
 opcode :: State IntState Int64
-opcode = do (_, _, m, pc, _) <- get
-            return $ m ! pc
+opcode = gets currentOp
 
 fetch :: Int64 -> State IntState Int64
-fetch addr = do (_, _, m, _, _) <- get
+fetch addr = do m <- gets memory
                 return $ if addr < 0 then error("Negative address")
                                      else Map.findWithDefault 0 addr m
 
 jmp :: (Int64 -> Int64) -> State IntState ()
-jmp f = do (i, o, m, pc, b) <- get
-           put (i, o, m, f pc, b)
+jmp f = do s <- get
+           put $ s { pc = f (pc s)}
 
 store :: Address -> Int64 -> State IntState ()
-store a v = do (i, o, m, pc, b) <- get
-               put (i, o, Map.insert a v m, pc, b)
+store a v = modify $ \s -> s { memory = Map.insert a v (memory s) }
 
 addr :: Int64 -> State IntState Int64
 addr n = do op <- opcode
-            (_, _, m, pc, b) <- get
+            b <- gets base
+            pc' <- gets pc
             let mode = op `div` 10 ^ (n + 1) `mod` 10
-            case mode of 2 -> (+ b) <$> fetch (pc + n)
-                         1 -> return (pc + n)
-                         0 -> fetch (pc + n)
+            case mode of 2 -> (+ b) <$> fetch (pc' + n)
+                         1 -> return (pc' + n)
+                         0 -> fetch (pc' + n)
 
 withBase :: Int64 -> State IntState ()
-withBase b = do (i, o, m, pc, _) <- get
-                put (i, o, m, pc, b)
+withBase b = modify $ \s -> s { base = b }
 
 setBase :: State IntState ()
-setBase = do (_, _, _, _, b) <- get
+setBase = do b <- gets base
              a <- addr 1 >>= fetch
              withBase $ a + b
              jmp (+ 2)
 
 readIo :: State IntState Int64
-readIo = do (i, o, m, pc, b) <- get
-            _ <- put (tail i, o, m, pc, b)
+readIo = do i <- gets input
+            _ <- modify $ \s -> s { input = tail i }
             return $ head i
 
 writeIo :: Int64 -> State IntState ()
-writeIo x = do (i, o, m, pc, b) <- get
-               put (i, x : o, m, pc, b)
+writeIo x = modify $ \s -> s { output = x : (output s) }
 
 binaryOp :: (Int64 -> Int64 -> Int64) -> State IntState ()
 binaryOp op = do a <- addr 1 >>= fetch
@@ -74,25 +83,25 @@ jif p = do a <- addr 1 >>= fetch
            if p a then jmp (const b)
                   else jmp (+ 3) 
 
-input :: State IntState ()
-input = do a <- addr 1
-           v <- readIo
-           _ <- store a v
-           jmp (+ 2)
+readInput :: State IntState ()
+readInput = do a <- addr 1
+               v <- readIo
+               _ <- store a v
+               jmp (+ 2)
 
-output :: State IntState ()
-output = do (_, o, _, _, _) <- get
-            a <- addr 1 >>= fetch
-            _ <- writeIo a
-            jmp (+ 2)
+writeOutput :: State IntState ()
+writeOutput = do o <- gets output
+                 a <- addr 1 >>= fetch
+                 _ <- writeIo a
+                 jmp (+ 2)
 
 stepS :: State IntState ()
 stepS = do op <- (`mod` 100) <$> opcode
            case op
              of 1 -> binaryOp (+)
                 2 -> binaryOp (*)
-                3 -> input
-                4 -> output
+                3 -> readInput
+                4 -> writeOutput
                 5 -> jif (/= 0)
                 6 -> jif (== 0)
                 7 -> binaryOp (\a b -> if a < b then 1 else 0)
@@ -105,28 +114,24 @@ step = execState stepS
 
 run :: (In, Memory) -> IntState 
 run = head . dropWhile notEnd . iterate step . initComputer
-       where initComputer = \im -> case im of (i, m) -> (i, [], m, 0, 0)
-             currentOp = \state -> case state of (_, _, m, pc, _) -> m ! pc
-             notEnd = (/= 99) . currentOp
+       where initComputer = \im -> case im of (i, m) -> IntState { input = i
+                                                                 , output = []
+                                                                 , memory = m
+                                                                 , pc = 0
+                                                                 , base = 0
+                                                                 }
+             notEnd = not . halted
 
 runO :: IntState -> IntState
 runO = head . dropWhile nonBlocking . iterate step
-       where currentOp = \state -> case state of (_, _, m, pc, _) -> m ! pc
-             nonBlocking s = notEnd s && (null . output $ s)
-             output s = let (_, o, _, _, _) = s in o
-             notEnd = (/= 99) . currentOp
+       where nonBlocking s = notEnd s && (null . output $ s)
+             notEnd = not . halted
 
 toMemory :: [Int64] -> Memory
 toMemory = Map.fromList . zip [0..]
-
-memory :: IntState -> Memory
-memory (_, _, m, _, _) = m
-
-getOutput :: IntState -> Out
-getOutput (_, o, _, _, _) = o
 
 runMem :: [Int64] -> [Int64]
 runMem = Map.elems . memory . run . (\m -> ([], m)) . toMemory
 
 runInOut :: [Int64] -> [Int64] -> [Int64]
-runInOut m i = reverse . getOutput . run . (\m' -> (i, m')) . toMemory $ m
+runInOut m i = reverse . output . run . (\m' -> (i, m')) . toMemory $ m
