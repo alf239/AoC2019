2201d46f3494c931db2565cdbb3fb160944f9d41
Alexey Filippov <afilippov@acm.org>
2019-12-09 17:03:13 +0000

    A typo

--- intcode/src/HSpecTests.hs
+++ intcode/src/HSpecTests.hs
@@ -64,8 +64,8 @@ main = hspec $ do
 
   describe "Day 9 - example" $ do
     it "outputs itself" $ do
-      let quince = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
-      runInOut quince [] `shouldBe` [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+      let quine = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+      runInOut quine [] `shouldBe` [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
 
     it "outputs 16-digit number" $ do
        (length . show . head $ runInOut [1102,34915192,34915192,7,4,7,99,0] []) `shouldBe` 16
