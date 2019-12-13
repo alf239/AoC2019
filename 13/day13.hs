43077436f8f6edd606333ce6494a235322620fc6
Alexey Filippov <afilippov@acm.org>
2019-12-13 05:09:46 +0000

    Day 13. Task 1

--- 13/day13.hs
+++ 13/day13.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import Control.Monad
+import qualified Data.Map.Strict as M
+import Data.List.Split
+import Intcode
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do input <- map read . splitOn "," <$> getContents
+
+          putStrLn "=== Task 1 ==="
+          let out = runInOut input []
+          let board = M.fromList $ [((x, y), z) | [x,y,z] <- chunksOf 3 out]
+         
+          print $ length . filter ((== 2) . snd) . M.toList $ board
+
