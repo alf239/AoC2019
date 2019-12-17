3cb292e75842cba516a1582f3b1bb2deabfd026b
Alexey Filippov <afilippov@acm.org>
2019-12-17 10:25:58 +0000

    Day 17, task 2: path encoding and simple output

--- 17/hay17.hs
+++ 17/hay17.hs
@@ -21,14 +21,20 @@ main = do input <- map read . splitOn "," <$> getContents
 
           putStrLn "=== Task 1 ==="
           let out = map (chr . fromIntegral) . runInOut input  $ []
-          let map = Set.fromList [(x, y) | (l, y) <- zip (lines out) [0..], (c, x) <- zip l [0..], scaffold c]
+          let mm = Set.fromList [(x, y) | (l, y) <- zip (lines out) [0..], (c, x) <- zip l [0..], scaffold c]
           putStrLn out
-          let iss = [(x, y) | (x, y) <- Set.toList map
-                            , Set.member (x+1, y) map
-                            , Set.member (x-1, y) map
-                            , Set.member (x, y+1) map
-                            , Set.member (x, y-1) map
+          let iss = [(x, y) | (x, y) <- Set.toList mm
+                            , Set.member (x+1, y) mm
+                            , Set.member (x-1, y) mm
+                            , Set.member (x, y+1) mm
+                            , Set.member (x, y-1) mm
                             ]
           print iss
           print $ sum [x * y | (x, y) <- iss]
 
+          putStrLn "=== Task 2 ==="
+          let program = "A,A,B,C,B,C,B,C,C,A\nL,10,R,8,R,8\nL,10,L,12,R,8,R,10\nR,10,L,12,R,10\nn\n" -- Feel free to switch the "video feed" to 'y'
+          let inp = map (fromIntegral . ord) program
+          let results = runInOut (2 : tail input) inp
+          putStrLn $ map (chr . fromIntegral) (init results)
+          print $ last results
