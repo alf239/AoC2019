3b96539b2b04dec79d5a400fe99df1fca2567b9f
Alexey Filippov <afilippov@acm.org>
2019-12-23 03:59:04 +0000

    Day 22 - cleanup

--- 22/day22.hs
+++ 22/day22.hs
@@ -29,19 +29,20 @@ gcdExt a b =
 
 type Shuffle = (Integer, Integer)
 
-modadd :: Integer -> Integer -> Integer -> Integer
-modadd n a b = (a + b) `mod` n
-
-modmul :: Integer -> Integer -> Integer -> Integer
-modmul n a b = (a * b) `mod` n
-
-modneg :: Integer -> Integer -> Integer
-modneg n 0 = 0
-modneg n a = n - a
-
 normalize :: Integer -> Integer -> Integer
 normalize n x | x < 0     = x + n
               | otherwise = x
+
+modop :: Integer -> (Integer -> Integer -> Integer) -> Integer -> Integer -> Integer
+modop n op a b = (a' `op` b') `mod` n
+               where a' = normalize n a
+                     b' = normalize n b
+
+modadd :: Integer -> Integer -> Integer -> Integer
+modadd n = modop n (+)
+
+modmul :: Integer -> Integer -> Integer -> Integer
+modmul n = modop n (*)
 
 shzero :: Shuffle
 shzero = (1, 0)
@@ -57,20 +58,19 @@ power n a b = shcompose n a' $! shcompose n p' p'
               where p' = power n a (b `div` 2)
                     a' = if odd b then a else shzero
 
+new_stack :: Shuffle
+new_stack = (-1, -1)
 
-new_stack :: Integer -> Shuffle
-new_stack n = (modneg n 1, modneg n 1)
+increment :: Integer -> Shuffle
+increment i = (i, 0)
 
-increment :: Integer -> Integer -> Shuffle
-increment n i = (i, 0)
+cut :: Integer -> Shuffle
+cut m = (1, -m)
 
-cut :: Integer -> Integer -> Shuffle
-cut n m = (1, modneg n (normalize n m))
-
-command :: Integer -> String -> Shuffle
-command n "deal into new stack"   = new_stack n
-command n s | dwi_ `isPrefixOf` s = increment n (read . drop (length dwi_) $ s)
-            | cut_ `isPrefixOf` s = cut       n (read . drop (length cut_) $ s)
+command :: String -> Shuffle
+command "deal into new stack"   = new_stack
+command s | dwi_ `isPrefixOf` s = increment (read . drop (length dwi_) $ s)
+          | cut_ `isPrefixOf` s = cut (read . drop (length cut_) $ s)
     where dwi_ = "deal with increment "
           cut_ = "cut "
 
@@ -81,23 +81,23 @@ shuffle :: Integer -> Shuffle -> Integer -> Integer
 shuffle n (a, b) k = modadd n (modmul n a k) b
 
 unshuffle :: Integer -> Shuffle -> Integer -> Integer 
-unshuffle n (a, b) k = modmul n a' (modadd n k (modneg n b))
+unshuffle n (a, b) k = modmul n a' (modadd n k (negate b))
                        where Just a' = modInv a n
 
 main :: IO ()
 main = do rules <- lines <$> getContents
+          let commands = map command rules
 
           putStrLn "=== Tests ==="
-          print ("deal into new stack",       [shuffle 10 (new_stack 10)              x | x <- [0..9]])
-          print ("deal into new stack twice", [shuffle 10 (power 10 (new_stack 10) 2) x | x <- [0..9]])
-          print ("cut 3",                     [shuffle 10 (cut 10 3)                  x | x <- [0..9]])
-          print ("cut 3",                     [shuffle 10 (power 10 (cut 10 1) 3)     x | x <- [0..9]])
-          print ("cut -4",                    [shuffle 10 (cut 10 (0-4))              x | x <- [0..9]])
-          print ("deal with increment 3",     [shuffle 10 (increment 10 3)            x | x <- [0..9]])
+          print ("deal into new stack",       [shuffle 10 new_stack              x | x <- [0..9]])
+          print ("deal into new stack twice", [shuffle 10 (power 10 new_stack 2) x | x <- [0..9]])
+          print ("cut 3",                     [shuffle 10 (cut 3)                x | x <- [0..9]])
+          print ("cut 3",                     [shuffle 10 (power 10 (cut 1) 3)   x | x <- [0..9]])
+          print ("cut -4",                    [shuffle 10 (cut (negate 4))       x | x <- [0..9]])
+          print ("deal with increment 3",     [shuffle 10 (increment 3)          x | x <- [0..9]])
 
           putStrLn "=== Task 1 ==="
           let n = 10007
-          let commands = map (command n) rules
           let result = compose_all n commands
           print $ shuffle n result 2019
           print $ unshuffle n result 5540
@@ -105,8 +105,7 @@ main = do rules <- lines <$> getContents
           putStrLn "=== Task 2 ==="
           let nn = 119315717514047
           let rr = 101741582076661
-          let commands2 = map (command nn) rules
-          let partial = compose_all nn commands2
+          let partial = compose_all nn commands
           let result2 = power nn partial rr
           let unshuffled = unshuffle nn result2 2020
           print unshuffled
