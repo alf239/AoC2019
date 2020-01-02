575ff811c8354c343238283e6dd6c285625658f7
Alexey Filippov <afilippov@acm.org>
2020-01-02 01:09:50 +0000

    Day 23 task 2: async channels

--- 23/day23.hs
+++ 23/day23.hs
@@ -6,31 +6,85 @@ import Data.List.Split (splitOn, chunksOf)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (isNothing)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Control.Concurrent (forkIO, yield)
+import Control.Concurrent.Chan.Unagi.NoBlocking
+import Control.Exception (handle)
+import Control.Monad
 import Control.Monad.Loops
 import Control.Monad.State.Strict
+import Debug.Trace
 
-nic :: Value -> [Value] -> IntState
-nic i = fromInMemory [i, -1]
+type InMessage = (Value, Value)
+type OutMessage = Either Value (Value, Value, Value)
+type ChannelIo = StateT (Bool, Maybe Value, Stream InMessage, Out) IO
+                           
+readCh :: Int -> InChan OutMessage -> ChannelIo Value
+readCh id o = do (ready, ib, str, ob) <- get
+                 if ready then case ib of
+                                    (Just x) -> do put (True, Nothing, str, ob)
+                                                   return x
+                                    Nothing  -> do msg <- liftIO . tryReadNext $ str
+                                                   case msg of
+                                                       Next (a, b) str' -> do put (True, Just b, str', ob)
+                                                                              return a
+                                                       Pending          -> do liftIO . writeChan o $ Left (fromIntegral id)
+                                                                              return $ negate 1
+                          else do put (True, ib, str, ob)
+                                  return (fromIntegral id)
 
-add_input :: IntState -> [Value] -> IntState
-add_input s i =  s { input = takeWhile (> 0) (input s) ++ i }
+writeCh :: InChan OutMessage -> Value -> ChannelIo ()
+writeCh ch a = do (_, _, _, buffer) <- get
+                  case buffer of
+                    [b, c]    -> do liftIO . writeChan ch $ Right (c, b, a)
+                                    modify $ \(i, ib, str, _) -> (i, ib, str, [])
+                    otherwise -> modify $ \(i, ib, str, _) -> (i, ib, str, a : buffer)
 
-step_all :: Map Int IntState -> (Map Int IntState, Maybe [Value])
-step_all nics = let step1 = [(k, runState runI s) | (k, s) <- Map.toList nics]
-                    msgs = Map.fromListWith (++) [(to, [x, y]) | (_, (out, _)) <- step1, [to, x, y] <- chunksOf 3 out]
-                    step1res = Map.fromAscList [(k, add_input s $ Map.findWithDefault [] (fromIntegral k) msgs) | (k, (_, s)) <- step1]
-                in (step1res, Map.lookup 255 msgs)
+runRW' :: ChannelIo Value -> (Value -> ChannelIo ()) -> IntCodeT ChannelIo ()
+runRW' rd wr = do whileM (gets $ not . halted) $! do stepRW rd wr
+                                                     liftIO yield
+                  return ()
+
+runCh :: IntState -> Int -> OutChan InMessage -> InChan OutMessage -> IO ()
+runCh ic id i o = let rd = readCh id o
+                      wr = writeCh o
+                      state = runIntCodeT (runRW' rd wr) ic
+                  in do [str] <- streamChan 1 i
+                        result <- evalStateT state (False, Nothing, str, [])
+                        return $ fst result
+
+
+consume :: Stream OutMessage -> [InChan InMessage] -> StateT (InMessage, Set Value) IO ()
+consume str cs  = do msg <- liftIO . tryReadNext $ str
+                     case msg of 
+                       Pending -> do (xy, stale) <- get
+                                     when (Set.size stale == length cs) $ do put (xy, Set.empty)
+                                                                             liftIO . writeChan (head cs) $ xy
+                                                                             liftIO . print $ ("Stalling", xy)
+                                     liftIO yield
+                                     consume str cs
+                       Next (Right (255, x, y)) str' -> do modify $ \(_, stale) -> ((x, y), stale)
+                                                           liftIO . print $ ("NAT update", (x, y))
+                                                           consume str' cs
+                       Next (Right (id, x, y)) str' -> do modify $ \(xy, stale) -> (xy, Set.delete id stale)
+                                                          liftIO . writeChan (cs !! fromIntegral id) $ (x, y)
+                                                          consume str' cs
+                       Next (Left id) str'  -> do modify $ \(xy, stale) -> (xy, Set.insert id stale)
+                                                  consume str' cs
+
+coordinate :: OutChan OutMessage -> [InChan InMessage] -> StateT (InMessage, Set Value) IO ()
+coordinate ch cs = do [str] <- liftIO . streamChan 1 $ ch
+                      consume str cs
+
 
 main :: IO ()
-main = do code <- map read . splitOn "," <$> getContents
+main = do baseNic <- fromMemory . map read . splitOn "," <$> getContents
+          (ci, co) <- newChan
 
-          let nics = Map.fromAscList [((fromIntegral i), nic i code) | i <- [0..49]]
+          cs <- forM [0..49] $ \id ->
+                do (i, o) <- newChan
+                   _ <- forkIO $ runCh baseNic id o ci
+                   return i
 
-          let steps = iterate (step_all . fst) (nics, Nothing)
-
-          let interesting = snd . head . dropWhile (isNothing . snd) $ steps :: Maybe [Value]
-
-          print interesting
-
-
-
+          evalStateT (coordinate co cs) ((-1, -1), Set.empty)
