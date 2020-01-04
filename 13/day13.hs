e8f665f19deb7ae2dacf41d85bfc76608921fa44
Alexey Filippov <afilippov@acm.org>
2020-01-04 01:13:28 +0000

    Day 13 task 2: first take, WIP

--- 13/day13.hs
+++ 13/day13.hs
@@ -1,20 +1,94 @@
 module Main where
 
-import Control.Monad
-import qualified Data.Map.Strict as M
-import Data.List.Split
-import Intcode
+import           Control.Monad
+import           Control.Monad.State.Strict
+import qualified Data.Map.Strict               as M
+import           Data.List.Split
+import           Intcode
+import           System.IO                      ( hFlush
+                                                , stdout
+                                                )
+import           System.Console.ANSI
+
+data Board = Board {
+    buffer :: [Int],
+    score  :: Value,
+    paddle :: Int,
+    ball   :: (Int, Int)
+}
+
+type Arkanoid = StateT Board IO
+
+block :: Value -> Char
+block 0 = ' '
+block 1 = '%'
+block 2 = '#'
+block 3 = '='
+block 4 = 'o'
+
+drawTile :: Int -> Int -> Char -> IO ()
+drawTile r c ch = do
+    setCursorPosition r c
+    putStr [ch]
+    hFlush stdout
+
+
+drawScore :: Value -> IO ()
+drawScore a = do
+    setCursorPosition 3 53
+    putStr (show a)
+    hFlush stdout
+
+readA :: Arkanoid Value
+readA = do
+    p        <- gets paddle
+    (bx, by) <- gets ball
+    let dir = if p > bx then 1 else (if p < bx then -1 else 0)
+    return $ if by > 23 then negate dir else dir
+
+writeA :: Value -> Arkanoid ()
+writeA a = do
+    buf <- gets buffer
+    case buf of
+        [0, -1] -> do
+            liftIO $ drawScore a
+            modify $ \s -> s { score = a, buffer = [] }
+        [b, c] -> do
+            liftIO $ drawTile b c (block $ fromIntegral a)
+            case a of
+                3         -> modify $ \s -> s { paddle = c, buffer = [] }
+                4         -> modify $ \s -> s { ball = (c, b), buffer = [] }
+                otherwise -> modify $ \s -> s { buffer = [] }
+        otherwise -> modify $ \s -> s { buffer = fromIntegral a : buf }
+
+runGame :: [Value] -> IO Value
+runGame m =
+    let init      = fromMemory m
+        intCode   = execStateT (runProgram readA writeA) init
+        ioProgram = execStateT intCode $ Board { buffer = []
+                                               , score  = -1
+                                               , paddle = -1
+                                               , ball   = (-1, -1)
+                                               }
+    in  score <$> ioProgram
 
 main :: IO ()
-main = do input <- map read . splitOn "," <$> getContents
+main = do
+    code <- map read . splitOn "," <$> getContents
 
-          putStrLn "=== Task 1 ==="
-          let out = runInOut input []
-          let board = M.fromList $ [((x, y), z) | [x,y,z] <- chunksOf 3 out]
-         
-          print $ length . filter ((== 2) . snd) . M.toList $ board
+    clearScreen
+    setCursorPosition 1 45
 
-          putStrLn "=== Task 2 ==="
-          let out1 = runInOut (2 : tail input) [1,1..]
-          let board1 = M.fromList $ [((x, y), z) | [x,y,z] <- chunksOf 3 out1]
-          print $ last . filter ((== (-1, 0)) . fst) . M.toList $ board1
+    putStr "Task 1: "
+    let out   = runInOut code []
+    let board = M.fromList $ [ ((x, y), z) | [x, y, z] <- chunksOf 3 out ]
+
+    putStr $ show . length . filter ((== 2) . snd) . M.toList $ board
+    hFlush stdout
+
+    setCursorPosition 3 45
+    putStr "Task 2: "
+    let patched = 2 : tail code
+    finalScore <- runGame patched
+
+    setCursorPosition 26 0
--- intcode/src/Intcode.hs
+++ intcode/src/Intcode.hs
@@ -1,13 +1,15 @@
 module Intcode where
 
-import Control.Monad.Loops (whileM)
-import Control.Monad.State.Strict
-import Control.Monad.Writer.Lazy
-import Data.Functor.Identity (Identity)
-import Data.Int (Int64)
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict ((!), Map)
-import Debug.Trace
+import           Control.Monad.Loops            ( whileM_ )
+import           Control.Monad.State.Strict
+import           Control.Monad.Writer.Lazy
+import           Data.Functor.Identity          ( Identity )
+import           Data.Int                       ( Int64 )
+import qualified Data.Map.Strict               as Map
+import           Data.Map.Strict                ( (!)
+                                                , Map
+                                                )
+import           Debug.Trace
 
 type Value = Int64
 type Memory = Map Value Value
@@ -33,95 +35,108 @@ opcode :: Monad m => IntCodeT m Value
 opcode = gets currentOp
 
 fetch :: Monad m => Address -> IntCodeT m Value
-fetch addr = do m <- gets memory
-                return $ if addr < 0 then error "Negative address"
-                                     else Map.findWithDefault 0 addr m
+fetch addr = do
+  m <- gets memory
+  return $ if addr < 0
+    then error "Negative address"
+    else Map.findWithDefault 0 addr m
 
 jmp :: Monad m => (Address -> Address) -> IntCodeT m ()
-jmp f = modify $ \s -> s { pc = f (pc s)}
+jmp f = modify $ \s -> s { pc = f (pc s) }
 
 store :: Monad m => Address -> Value -> IntCodeT m ()
 store a v = modify $ \s -> s { memory = Map.insert a v (memory s) }
 
 addr :: Monad m => Int -> IntCodeT m Value
-addr n = do op <- opcode
-            b <- gets base
-            pc' <- gets pc
-            let mode = op `div` 10 ^ (n + 1) `mod` 10
-            let param = pc' + fromIntegral n
-            case mode of 2 -> (+ b) <$> fetch param
-                         1 -> return param
-                         0 -> fetch param
+addr n = do
+  op  <- opcode
+  b   <- gets base
+  pc' <- gets pc
+  let mode  = op `div` 10 ^ (n + 1) `mod` 10
+  let param = pc' + fromIntegral n
+  case mode of
+    2 -> (+ b) <$> fetch param
+    1 -> return param
+    0 -> fetch param
 
 setBase :: Monad m => IntCodeT m ()
-setBase = do a <- addr 1 >>= fetch
-             _ <- modify $ \s -> s { base = a + base s }
-             jmp (+ 2)
+setBase = do
+  a <- addr 1 >>= fetch
+  _ <- modify $ \s -> s { base = a + base s }
+  jmp (+ 2)
 
 binaryOp :: Monad m => (Value -> Value -> Value) -> IntCodeT m ()
-binaryOp op = do a <- addr 1 >>= fetch
-                 b <- addr 2 >>= fetch
-                 c <- addr 3
-                 _ <- store c (a `op` b)
-                 jmp (+ 4)
+binaryOp op = do
+  a <- addr 1 >>= fetch
+  b <- addr 2 >>= fetch
+  c <- addr 3
+  _ <- store c (a `op` b)
+  jmp (+ 4)
 
 jif :: Monad m => (Value -> Bool) -> IntCodeT m ()
-jif p = do a <- addr 1 >>= fetch
-           b <- addr 2 >>= fetch
-           let target = if p a then const b else (+ 3)
-           jmp target
+jif p = do
+  a <- addr 1 >>= fetch
+  b <- addr 2 >>= fetch
+  let target = if p a then const b else (+ 3)
+  jmp target
 
 readInput :: Monad m => m Value -> IntCodeT m ()
-readInput rd = do a <- addr 1
-                  v <- lift rd
-                  _ <- store a v
-                  jmp (+ 2)
+readInput rd = do
+  a <- addr 1
+  v <- lift rd
+  _ <- store a v
+  jmp (+ 2)
 
 writeOutput :: Monad m => (Value -> m ()) -> IntCodeT m ()
-writeOutput write = do a <- addr 1 >>= fetch
-                       _ <- lift $ write a
-                       jmp (+ 2)
+writeOutput write = do
+  a <- addr 1 >>= fetch
+  _ <- lift $ write a
+  jmp (+ 2)
 
 noop :: Monad m => IntCodeT m ()
 noop = jmp (+ 1)
 
 step :: Monad m => m Value -> (Value -> m ()) -> IntCodeT m ()
-step rd wr = do op <- (`mod` 100) <$> opcode
-                case op
-                  of 0 -> noop
-                     1 -> binaryOp (+)
-                     2 -> binaryOp (*)
-                     3 -> readInput rd
-                     4 -> writeOutput wr
-                     5 -> jif (/= 0)
-                     6 -> jif (== 0)
-                     7 -> binaryOp (\a b -> if a < b then 1 else 0)
-                     8 -> binaryOp (\a b -> if a == b then 1 else 0)
-                     9 -> setBase
+step rd wr = do
+  op <- (`mod` 100) <$> opcode
+  case op of
+    0 -> noop
+    1 -> binaryOp (+)
+    2 -> binaryOp (*)
+    3 -> readInput rd
+    4 -> writeOutput wr
+    5 -> jif (/= 0)
+    6 -> jif (== 0)
+    7 -> binaryOp (\a b -> if a < b then 1 else 0)
+    8 -> binaryOp (\a b -> if a == b then 1 else 0)
+    9 -> setBase
 
 runProgram :: Monad m => m Value -> (Value -> m ()) -> IntCodeT m ()
-runProgram rd wr = do whileM (gets $ not . halted) $ step rd wr
-                      return ()
+runProgram r w = whileM_ (gets $ not . halted) $ step r w
 
 readIoS :: BasicIC Value
-readIoS = do i <- gets head
-             _ <- modify tail
-             return i
+readIoS = do
+  i <- gets head
+  _ <- modify tail
+  return i
 
 writeIoS :: Value -> BasicIC ()
 writeIoS a = lift $ tell [a]
 
-fromMemory :: [Value] -> IntState 
-fromMemory m = IntState { memory = Map.fromAscList . zip [0..] $ m, pc = 0, base = 0 }
+fromMemory :: [Value] -> IntState
+fromMemory m =
+  IntState { memory = Map.fromAscList . zip [0 ..] $ m, pc = 0, base = 0 }
 
 runMem :: [Value] -> [Value]
-runMem m = let init           = fromMemory m
-               intCode        = execStateT (runProgram readIoS writeIoS) init
-               processedInput = evalStateT intCode []
-           in Map.elems . memory . fst . runWriter $ processedInput
+runMem m =
+  let init           = fromMemory m
+      intCode        = execStateT (runProgram readIoS writeIoS) init
+      processedInput = evalStateT intCode []
+  in  Map.elems . memory . fst . runWriter $ processedInput
 
 runInOut :: [Value] -> In -> Out
-runInOut m i = let init           = fromMemory m
-                   intCode        = runStateT (runProgram readIoS writeIoS) init
-                   processedInput = runStateT intCode i
-               in execWriter processedInput
+runInOut m i =
+  let init           = fromMemory m
+      intCode        = execStateT (runProgram readIoS writeIoS) init
+      processedInput = runStateT intCode i
+  in  execWriter processedInput
